ad8c4947df356d3fd655c6e63481424c44292e42
sosku <61566439+sosku@users.noreply.github.com>
2024-04-27 00:19:23 +0000

    y

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-link = 'magnet:paste-magnet-link-here';
+link = 'magnet:?xt=urn:btih:TVKCGWZCVHZNVL6APYTII34YD5SHUTPL&dn=AndoliniXXL%20Fucks%20Marocain21cm&tr=http%3A%2F%2Ftracker.gay-torrents.net%3A6970%2F18bb1fff00978d0f12cc67d8de985624%2Fannounce';
 process.argv = ["cli", "download", link, "--keep-seeding"];
 require('./node_modules/webtorrent-cli/bin/cmd');
